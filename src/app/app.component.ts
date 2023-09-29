500d7380b9f0d12e7df70964e7f70a74502204a0
Rajani VC <112753940+rajaniamin@users.noreply.github.com>
2023-09-29 10:36:33 +0530

    updatedcode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'weatherApplication';
+  cityName?:string;
+
+  onSubmit(){
+
+  }
 }
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment'; // Import environment
 import { Environment } from 'src/environments/environment'; // Import Environment type
@@ -14,7 +14,11 @@ export class WeatherService {
     this.http.get(environmentConfig.weatherApiBaseUrl, {
       headers: new HttpHeaders()
         .set(environmentConfig.XRapidAPIHostHeaderName, environmentConfig.XRapidAPIHostValue)
-        .set(environmentConfig.XRapidAPIHeaderName, environmentConfig.XRapidAPIValue)
+        .set(environmentConfig.XRapidAPIHeaderName, environmentConfig.XRapidAPIValue),
+        params : new HttpParams()
+        .set('q',cityName)
+        .set('units','metric')
+        .set('mode','json')
     }).subscribe(
       (data) => {
         console.log('Weather data:', data);
